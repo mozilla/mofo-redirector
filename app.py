656a44eec142b44a4aba5df89b5f4956aa9d3ec9
Lucie Daeye <lucied@mozillafoundation.org>
2019-07-29 18:53:59 +0200

    Add a way to pass tests config

--- app.py
+++ app.py
@@ -11,83 +11,90 @@ from flask import (
     abort,
 )
 
-app = Flask(__name__, static_folder=None)
 
-app.config.from_object('config.Config')
+def create_app(test_config=None):
+    app = Flask(__name__, static_folder=None)
 
-redirect_rules = app.config['REDIRECT_RULES']
-force_ssl = app.config['FORCE_SSL']
-debug = app.config['DEBUG']
-
-
-@app.before_request
-def enforce_ssl():
-    if not force_ssl:
-        return None
-
-    proto = request.headers.get('X-Forwarded-Proto', None)
-
-    if proto == 'https':
-        return None
-
-    url = request.url.replace('http://', 'https://', 1)
-    return redirect(url, code=301)
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def redirector(path):
-    x_forwarded_host = request.headers.get('X-Forwarded-Host', None)
-
-    if x_forwarded_host:
-        host = x_forwarded_host
+    if test_config is None:
+        app.config.from_object('config.Config')
     else:
-        host = request.headers.get('Host', None)
+        app.config.update(test_config)
 
-    if debug:
-        print('received request from {}'.format(host))
+    redirect_rules = app.config['REDIRECT_RULES']
+    force_ssl = app.config['FORCE_SSL']
+    debug = app.config['DEBUG']
 
-    if host in redirect_rules:
-        redirect_target, redirect_code, preserves = redirect_rules[host]
-        preserve_path, preserve_query = preserves
 
-        redirect_path = ''
-        redirect_query = ''
+    @app.before_request
+    def enforce_ssl():
+        if not force_ssl:
+            return None
 
-        target_url = urlparse(redirect_target)
+        proto = request.headers.get('X-Forwarded-Proto', None)
 
-        if preserve_path:
-            redirect_path = path
+        if proto == 'https':
+            return None
+
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def redirector(path):
+        x_forwarded_host = request.headers.get('X-Forwarded-Host', None)
+
+        if x_forwarded_host:
+            host = x_forwarded_host
         else:
-            redirect_path = target_url.path
-
-        if preserve_query:
-            redirect_query = urlencode(request.args, doseq=True)
-
-        redirect_parse = ParseResult(
-            scheme=target_url.scheme,
-            netloc=target_url.netloc,
-            path=redirect_path,
-            query=redirect_query,
-            params='',
-            fragment=''
-        )
-
-        final_redirect = urlunparse(redirect_parse)
+            host = request.headers.get('Host', None)
 
         if debug:
-            print('redirecting to {} with a {}'.format(final_redirect, redirect_code.value))
+            print('received request from {}'.format(host))
 
-        return redirect(final_redirect, code=redirect_code.value)
+        if host in redirect_rules:
+            redirect_target, redirect_code, preserves = redirect_rules[host]
+            preserve_path, preserve_query = preserves
 
-    return abort(400)
+            redirect_path = ''
+            redirect_query = ''
+
+            target_url = urlparse(redirect_target)
+
+            if preserve_path:
+                redirect_path = path
+            else:
+                redirect_path = target_url.path
+
+            if preserve_query:
+                redirect_query = urlencode(request.args, doseq=True)
+
+            redirect_parse = ParseResult(
+                scheme=target_url.scheme,
+                netloc=target_url.netloc,
+                path=redirect_path,
+                query=redirect_query,
+                params='',
+                fragment=''
+            )
+
+            final_redirect = urlunparse(redirect_parse)
+
+            if debug:
+                print('redirecting to {} with a {}'.format(final_redirect, redirect_code.value))
+
+            return redirect(final_redirect, code=redirect_code.value)
+
+        return abort(400)
 
 
-@app.after_request
-def response_headers(response):
-    response.headers['Server'] = 'MoFo Redirector'
-    return response
+    @app.after_request
+    def response_headers(response):
+        response.headers['Server'] = 'MoFo Redirector'
+        return response
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
